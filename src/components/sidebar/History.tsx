1d4ef624eb7ae26577b7d2cbbd83847cffb7adb9
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-15 21:51:08 +0700

    chore<>: remove the example array map

--- src/components/sidebar/History.tsx
+++ src/components/sidebar/History.tsx
@@ -1,18 +1,13 @@
 import HistoryItems from "./HistoryItems"
 import { ScrollArea } from "../ui/scroll-area"
-const tags = Array.from({ length: 50 }).map(
-   (_, i, a) => `v1.2.0-beta.${a.length - i}`
- )
+
 
 
 const History = async() => {
    return (
       <div className="flex flex-col">
          <ScrollArea className="h-[200px] w-full bord">
-            <HistoryItems data />
-            <HistoryItems data />
-            <HistoryItems data />
-            <HistoryItems data />
+            <HistoryItems />
          </ScrollArea>
       </div>
    )
