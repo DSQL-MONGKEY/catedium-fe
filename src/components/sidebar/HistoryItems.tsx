ded871fe0433976a517e281f38ed87770318cb9f
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-15 21:51:50 +0700

    feat<>: remove data props and change to histories global state to create the list

--- src/components/sidebar/HistoryItems.tsx
+++ src/components/sidebar/HistoryItems.tsx
@@ -1,12 +1,21 @@
+'use client'
+
+import { useHistoryStore } from "@/state"
 
 
 
-const HistoryItems = ({ data: String }) => {
+const HistoryItems = () => {
+   const { histories } = useHistoryStore()
+
    return (
       <div className="bg-slate-200 p-2 m-3 rounded">
-         <div className="">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Possimus libero accusamus fuga, a id aperiam quis doloremque dignissimos esse quia cumque quam illo ipsum velit ab fugit sed molestias obcaecati!
-         </div>
+         {histories.map((item) => (
+            <div key={item.requestId}>
+               <h3 className="text-xl text-black">
+                  {item.class}
+               </h3>
+            </div>
+         ))}
       </div>
    )
 }
