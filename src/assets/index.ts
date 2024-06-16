8e2c2f1448ff9a9e8518ddfad4964e80d3bb6ce1
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-16 12:43:11 +0700

    image assets

---
+++ src/assets/index.ts
@@ -0,0 +1,25 @@
+import africanLeo from './african-leopard.webp'
+import caracal from './caracal.webp'
+import cheetah from './cheetah.webp'
+import cloudedLeo from './clouded-leo.webp'
+import jaguar from './jaguar.webp'
+import lion from './lion.webp'
+import ocelot from './ocelot.webp'
+import puma from './puma.webp'
+import snowLeo from './snow-leopard.webp'
+import tiger from './tiger.webp' 
+
+export {
+   africanLeo,
+   caracal,
+   cheetah,
+   cloudedLeo,
+   jaguar,
+   lion,
+   ocelot,
+   puma,
+   snowLeo,
+   tiger
+}
+
+
