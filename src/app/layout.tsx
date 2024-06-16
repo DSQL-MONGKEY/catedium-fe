5a89a0824ecb98e1dc1504edbb84d6432e16d755
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-16 12:34:41 +0700

    feat<children>: add initiate toaster provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 const fontSans = Inter({
@@ -30,6 +31,7 @@ export default function RootLayout({
         disableTransitionOnChange
       >
         {children}
+        <Toaster/>
       </ThemeProvider>
       </body>
     </html>
