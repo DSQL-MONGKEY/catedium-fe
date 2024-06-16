09bb8ab1a6000cbb45b1bd82b4aa769356c9ee6a
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-16 12:42:18 +0700

    feat<map-loop>: add timestamp properties to display and fix layout problem

--- src/components/sidebar/HistoryItems.tsx
+++ src/components/sidebar/HistoryItems.tsx
@@ -2,18 +2,25 @@
 
 import { useHistoryStore } from "@/state"
 
-
-
 const HistoryItems = () => {
    const { histories } = useHistoryStore()
 
    return (
-      <div className="bg-slate-200 p-2 m-3 rounded">
+      <div className="flex flex-col gap-2 p-2 m-3 rounded">
          {histories.map((item) => (
-            <div key={item.requestId}>
-               <h3 className="text-xl text-black">
-                  {item.class}
-               </h3>
+            <div 
+               key={item.requestId}
+               className="px-2 py-3 rounded-md bg-white border border-slate-200 hover:border-slate-700 dark:border-white/[0.1] dark:bg-popover dark:hover:border-cyan-900 shadow-sm duration-100"
+            >
+               <div className="flex justify-between items-center">
+                  <h3 className="text-lg text-zinc-800 dark:text-white font-bold capitalize">
+                     {item.class.toLowerCase()}  
+                  </h3>
+                  &nbsp;
+                  <span className="text-xs text-slate-500 dark:text-zinc-400 font-semibold">
+                     {item.timestamp}
+                  </span>
+               </div>
             </div>
          ))}
       </div>
