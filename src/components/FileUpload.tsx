153f2d8da028406060bb9bc4d5856cb6412a9933
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-17 13:56:42 +0700

    feat<toaster>: change toaster from sonner

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -6,35 +6,39 @@ import { Button } from './ui/button'
 import { useHistoryStore } from '@/state'
 import { Progress } from './ui/progress'
 import { useToast } from './ui/use-toast'
-import { ToastAction } from './ui/toast'
-import { ToastTitle } from '@radix-ui/react-toast'
-import { cn } from '@/lib/utils'
+import { toast } from 'sonner'
 
 
 const FileUpload = () => {
    const [file, setFile] = useState<any>()
    const [progress, setProgress] = useState<number>(0)
    const { addHistories } = useHistoryStore()
-   const { toast } = useToast()
+   const topToast = useToast().toast
 
    const handleFilePredict = async() => {
       const formData = new FormData()
       formData.append('file', file)
-
       try {
          if(file) {
-            toast({
-               title: "Predicting...",
-               description: "Predicting the breeds on the image",
+            toast("Predicting...",{
+               description: `Predicting the breeds on ${file.name}`,
+               duration: 10000,
             })
             setProgress(20)
             setTimeout(() => setProgress(50), 2000)
             setTimeout(() => setProgress(80), 4000)
          }
          if(!file) {
-            return toast({
-               title: "Error",
-               description: "Please ensure you've chosen your image",
+            return toast("Error", {
+               description: "Please ensure the image you've chosen",
+               duration: 5000,
+               classNames: {
+                  error: 'bg-rose-500'
+               },
+               style: {
+                  background: 'red',
+                  color: 'white'
+               }
             })
          }
          
@@ -50,21 +54,22 @@ const FileUpload = () => {
          })
 
          if(data) {
-            toast({
-               title: 'Success',
-               description: 'The prediction proccess has been succeeded',
+            toast("Success", {
+               description: 'The prediction proccess succeeded',
+               duration: 7000,
             })
             setProgress(100)
             setTimeout(() => setProgress(0), 800)
-         }
-         console.log(data)
-         console.log(file)
-         
+
+            setTimeout(() => 
+               toast(`Rawrr! I'm ${data.data.class}`,{
+               description: `You can check the detail on list below`,
+               duration: 7000
+            }), 1000)
+         }         
       } catch(err: any) {
-         toast({
-            title: "Error",
+         toast("Error", {
             description: err.message,
-            variant: "destructive"
          })
       }
 
@@ -91,7 +96,7 @@ const FileUpload = () => {
          </div>
 
          <div>
-            <Button onClick={handleFilePredict} className="text-slate-400 text-sm">
+            <Button onClick={handleFilePredict} className="text-white dark:text-accent text-sm">
                Predict Image
             </Button>
          </div>
