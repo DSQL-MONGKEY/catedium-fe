ca4045328e0c1fc28f8dbe66fc5efd0bca17136c
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-17 14:41:18 +0700

    feat<Breed-name>: add conditional style class name

--- src/components/sidebar/HistoryItems.tsx
+++ src/components/sidebar/HistoryItems.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { cn } from "@/lib/utils"
 import { useHistoryStore } from "@/state"
 
 const HistoryItems = () => {
@@ -13,7 +14,7 @@ const HistoryItems = () => {
                className="px-2 py-3 rounded-md bg-white border border-slate-200 hover:border-slate-700 dark:border-white/[0.1] dark:bg-popover dark:hover:border-cyan-900 shadow-sm duration-100"
             >
                <div className="flex justify-between items-center">
-                  <h3 className="text-lg text-zinc-800 dark:text-white font-bold capitalize">
+                  <h3 className={cn('text-zinc-800 dark:text-white font-bold capitalize', item.class.length > 5 ? 'text-sm' : 'text-lg')}>
                      {item.class.toLowerCase()}  
                   </h3>
                   &nbsp;
