4fa4eb31d8d41c58829bc2e50b8f343aac80275d
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-17 13:54:36 +0700

    feat<toaster>: change toaster from sooner

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
 import { cn } from "@/lib/utils";
-import { Toaster } from "@/components/ui/toaster";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 const fontSans = Inter({
