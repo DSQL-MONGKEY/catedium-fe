a0d71ede7debb5d2205b2a2f9e9e6b2d9a759198
Dimpfe <dimasprasetyo7696@gmail.com>
2024-06-18 23:11:13 +0700

    style<>: change padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,18 +10,14 @@ import BreedsList from "@/components/BreedsList";
 export default function Home() {
 
   return (
-    <main className="w-full h-screen flex flex-row overflow-hidden gap-3 bg-white dark:bg-black px-10 py-6 ">
+    <main className="w-full h-screen flex flex-row overflow-hidden gap-3 bg-white dark:bg-black">
       <SideBar />
-      <div className="flex flex-col w-full h-full justify-center">
+      <div className="flex flex-col w-full h-full gap-3 justify-center px-10 py-3">
         <div className="w-full flex flex-col rounded-md justify-center items-center border border-slate-200 dark:border-gray-700 dark:bg-slate-900 shadow-sm py-10">
             <UploadField/>
         </div>
 
-        <div className="flex w-full">
-          
-        </div>
-
-        <div className="w-full mt-5 relative h-[400px] border border-slate-200 rounded-lg overflow-y-auto no-scrollbar shadow-sm dark:border-gray-700 dark:bg-slate-900">
+        <div className="w-full h-[400px] border border-slate-200 rounded-lg overflow-y-auto no-scrollbar shadow-sm dark:border-gray-700 dark:bg-slate-900">
           <BreedsList />
         </div>
       </div>
